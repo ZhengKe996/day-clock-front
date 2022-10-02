455c03bbe3d5231c83f338afcdd0bf6495c0012c
ZhengKe <zhengke996@163.com>
2022-10-02 11:40:40 +0800

    feat：优化细节

--- src/components/Detial/src/detial.vue
+++ src/components/Detial/src/detial.vue
@@ -2,9 +2,7 @@
   <div v-if="loading" class="w-full h-screen flex justify-center items-center">
     <a-spin size="large" tip="Loading..." :delay="200" />
   </div>
-  <template v-else>
-    <a-table :dataSource="dataSource" :columns="!isMobileTerminal ? columns : columnsMobile" :pagination="!isMobileTerminal" />
-  </template>
+  <template v-else> <a-table :dataSource="dataSource" :columns="!isMobileTerminal ? columns : columnsMobile" :pagination="!isMobileTerminal" :size="isMobileTerminal ? 'middle' : ''" /> </template>
 </template>
 
 <script setup lang="ts">
@@ -25,22 +23,26 @@ const columns: TableColumnType<TableDataType>[] = [
   {
     title: '年级',
     dataIndex: 'grade',
-    key: 'grade'
+    key: 'grade',
+    align: 'center'
   },
   {
     title: '班级',
     dataIndex: 'class',
-    key: 'class'
+    key: 'class',
+    align: 'center'
   },
   {
     title: '姓名',
     dataIndex: 'name',
-    key: 'name'
+    key: 'name',
+    align: 'center'
   },
   {
     title: '状态',
     dataIndex: 'progress',
-    key: 'progress'
+    key: 'progress',
+    align: 'center'
   }
 ]
 
@@ -48,17 +50,23 @@ const columnsMobile: TableColumnType<TableDataType>[] = [
   {
     title: '班级',
     dataIndex: 'class',
-    key: 'class'
+    key: 'class',
+    align: 'left',
+    ellipsis: true
   },
   {
     title: '姓名',
     dataIndex: 'name',
-    key: 'name'
+    key: 'name',
+    align: 'center',
+    width: 100
   },
   {
     title: '状态',
     dataIndex: 'progress',
-    key: 'progress'
+    key: 'progress',
+    align: 'center',
+    width: 100
   }
 ]
 </script>
